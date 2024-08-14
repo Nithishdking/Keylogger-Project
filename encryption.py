56da4d498c1a37557dee53f968f94bcf609efba3
Nithishdking <nithishdking@gmail.com>
2024-08-14 15:33:38 +0530

    Encryption

---
+++ encryption.py
@@ -0,0 +1,13 @@
+# encryption.py
+from cryptography.fernet import Fernet
+
+def generate_key():
+    return Fernet.generate_key()
+
+def encrypt_data(data, key):
+    fernet = Fernet(key)
+    return fernet.encrypt(data.encode())
+
+def decrypt_data(data, key):
+    fernet = Fernet(key)
+    return fernet.decrypt(data).decode()
